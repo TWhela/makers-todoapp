3b388c7024771c6d49f5e17213b292fda74a5946
Tom Whelan <thomas.whelan20@icloud.com>
2023-10-04 11:19:26 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Thomas Whelan on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
