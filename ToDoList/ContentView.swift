4cdd4c593a900fbe972e01ce430da29f4b63cddf
Tom Whelan <thomas.whelan20@icloud.com>
2023-10-05 15:54:46 +0100

    Created to do app

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var todoTextField: String = ""
+    @State var todoDataArray: [(String, Bool)] = []
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("todo")
+                .resizable()
+                .frame(width: 300, height: 200)
+            Text("To Do:")
+                .font(.largeTitle.bold())
+
+            HStack {
+                TextField("Text here...", text: $todoTextField)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .foregroundColor(.blue)
+                    .font(.headline)
+
+                Button(action: {
+                    if validInput() {
+                        saveTodo()
+                    }
+                }) {
+                    Text("Add to list")
+                        .fontWeight(.heavy)
+                        .background(validInput() ? Color(.systemBlue) : Color(.lightGray))
+                        .foregroundColor(.white)
+                        .cornerRadius(1)
+                }
+                .disabled(!validInput())
+
+                Button(action: {
+                    todoDataArray.removeAll() // Clear the todoDataArray
+                }) {
+                    Text("Clear All")
+                        .fontWeight(.heavy)
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(1)
+                }
+            }
+            .padding()
+
+            ForEach(todoDataArray.indices, id: \.self) { index in
+                Toggle(isOn: Binding(
+                    get: { self.todoDataArray[index].1 },
+                    set: { newValue in self.todoDataArray[index].1 = newValue }
+                )) {
+                    Text(self.todoDataArray[index].0)
+                        .font(.headline.bold())
+                }
+                .toggleStyle(SwitchToggleStyle(tint: Color(.systemBlue)))
+            }
         }
         .padding()
+    }
+
+    func validInput() -> Bool {
+        if todoTextField.count >= 3 {
+            return true
+        }
+        return false
+    }
+
+    func saveTodo() {
+        todoDataArray.append((todoTextField, false))
+        todoTextField = ""
     }
 }
 
